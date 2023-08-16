bd743e5bb70808706a3a88260ef6650a23c97a8b
enis-gerikoglu <enisgerikoglu@gmail.com>
2023-08-16 14:35:35 +0300

    modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -6,3 +6,31 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+const imgs = document.getElementById('imgs')
+
+const img = document.querySelectorAll('#imgs img')
+
+let idx = 0
+
+let interval = setInterval(run, 2000)
+
+function run() {
+    idx++
+    changeImage()
+}
+
+function changeImage() {
+    if(idx > img.length - 1) {
+        idx = 0
+    } else if(idx < 0) {
+        idx = img.length - 1
+    }
+
+    imgs.style.transform = `translateX(${-idx * 100}%)`
+}
+
+function resetInterval() {
+    clearInterval(interval)
+    interval = setInterval(run, 5000)
+}
+
