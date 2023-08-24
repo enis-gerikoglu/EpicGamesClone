073358c9e77457bd1bc5695eb7fff6452a87f955
enis-gerikoglu <enisgerikoglu@gmail.com>
2023-08-24 14:00:42 +0300

    renamed:    25BR_Quotient_EGS_Launcher_Blade_2560x1440_2560x1440-0dce844812533fa4feb32c88c4f371e5.avif -> images/25BR_Quotient_EGS_Launcher_Blade_2560x1440_2560x1440-0dce844812533fa4feb32c88c4f371e5.avif 	renamed:    83-833628_epic-games-logo-png-epic-games-logo-png.png -> images/83-833628_epic-games-logo-png-epic-games-logo-png.png 	renamed:    AC KINGDOM PREORDER_STANDARD EDITION_EPIC_Key_Art_Portrait_640x854-640x854-288120c5573756cb988b6c1968cebd86.avif -> images/AC KINGDOM PREORDER_STANDARD EDITION_EPIC_Key_Art_Portrait_640x854-640x854-288120c5573756cb988b6c1968cebd86.avif

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 let mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
-// When the user clicks on the button, scroll to the top of the document
+
+window.onscroll = function() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+};
+
+
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+ 
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
 }
 const imgs = document.getElementById('imgs')
 
